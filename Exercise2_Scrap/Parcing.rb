a2dfcf1727514eb8a6e469e7f3eec6b52e71b7de
Kevinbarrero <kevinbar98@gmail.com>
2021-09-23 01:15:49 +0300

    update homework 2

--- Exercise2_Scrap/Parcing.rb
+++ Exercise2_Scrap/Parcing.rb
@@ -2,10 +2,11 @@ require 'open-uri'
 require 'net/http'
 require 'nokogiri'
 require 'httparty'
-
+require 'webdrivers'
 html = URI.open("http://kazan-news.net/")
 #uri = URI.parse(html)
 response = Nokogiri::HTML(html)
 #puts response
-description = response.css('span').include('news-list__ntitle').text
-puts description
+response = response.xpath("//span[@class='news-list__ntitle']").text
+
+puts response
