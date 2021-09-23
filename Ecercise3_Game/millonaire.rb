45b723a06c589484a9a557f731f9e6a3499f87ea
Kevinbarrero <kevinbar98@gmail.com>
2021-09-23 13:57:41 +0300

    added homework 3

---
+++ Ecercise3_Game/millonaire.rb
@@ -0,0 +1,9 @@
+questions{"question"=>"In the UK, the abbreviation NHS stands for National what Service?",
+"answer"=> "Health"}, {"question"=>"Which of these brands was chiefly associated with the manufacture of household locks?",
+"answer"=> "Chubb"}, {"question"=>"Construction of which of these famous landmarks was completed first?",
+"answer"=> "Big Ben"}, {"question"=>"Which of these cetaceans is classified as a “toothed whale”?",
+"answer"=> "Sperm Whale"}, {"question"=>"which pirate died in battle off the coast of what is now North Carolina?",
+"answer"=> "Blackbeard"}
+
+
+puts questions
